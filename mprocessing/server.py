b9e8d13066d12ee779376fdfd24ac2b34ac73ba2
Fabian W. G. Transchel <fabiantranschel@web.de>
2016-04-19 14:50:42 +0200

    Update server.py
    Updated with some more usable interface to the server. Can now add jobs after starting the process.

--- mprocessing/server.py
+++ mprocessing/server.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This module is part of
+an extension of evoMPS by adding
+dissipative dynmaics based on
+Monte-Carlo methods.
+
+This part is the server file
+for the distributed computing
+framework that utilizes parallel
+processing to speed up dissipative
+dynamics.
+
+@author: F.W.G. Transchel
+"""
+
 import Queue as qu
 import multiprocessing as mp
 import multiprocessing.managers as mpm
@@ -10,21 +28,30 @@ import pickle as pic
 import numpy as np
 import scipy as sp
 import scipy.linalg as la
+import tdvp_common_diss as tm
 import nullspace as ns
-import matmul as m
+import matmul as mm
 import matplotlib.pyplot as plt
 
-authkey = "testcase"
-ip = "x.x.x.x"
-port = xxxx
+authkey = "SECRET"
+ip = "127.0.0.1"
+port = 5678
+internal_call = True
+global num_results
+num_results = 0
+global num_started_jobs
+num_started_jobs = 0
+global total_iterations
+global result_list
+result_list = []
 
 def do_calculations(shared_job_q,shared_result_q,codebase):
-    #nums = make_nums(N)
 
-    # The numbers are split into chunks. Each chunk is pushed into the job
-    # queue.
-    #chunksize = 3
-    #for i in range(0, len(nums), chunksize):
+    global num_results
+    global num_started_jobs
+    
+    num_results = 0
+    num_started_jobs = 0
             
     cdbs = codebase.__str__() + "\n"
     code = cdbs[12:-3]
@@ -37,105 +64,108 @@ def do_calculations(shared_job_q,shared_result_q,codebase):
     result_list = []
     
     # Measure execution time
-
-    t0 = time.time()    
     
     for i in xrange(1,total_iterations+1):
         print "Sending job#" + str(i)
         #shared_job_q.put(str("print \"test: This is process #" + str(i) + "/" + str(N) + "\"\ndef crunching_zombie(n):\n\treturn n*n\nresult = crunching_zombie("+str(i)+")\nprint result\n"))
-        shared_job_q.put("result = sample_path(EvoMPS_TDVP_Generic,"+str(i)+")")
+        shared_job_q.put("result = sample_path(evoMPS_TDVP_Generic_Dissipative,"+str(i)+")")
 
     print "==="
     # Wait until all results are ready in shared_result_q
-    numresults = 0
-    while numresults < total_iterations:
-        result_list.append(shared_result_q.get())
-        print "Got result (" + str(numresults+1) + "/" + str(total_iterations) + ")"
-        #print numresults
-        numresults += 1
-        if shared_result_q.empty():
-            #print "(Queue empty.)"
-            time.sleep(1)
-   
-    #print result_list         
-    print "==="
-    print "Computation complete."
-    print "Operation took " + str((time.time() - t0)) + " s."
+    num_results = 0
+    num_started_jobs = total_iterations
     
+    return do_calculation_prompt(shared_job_q,shared_result_q)
+
+def add_jobs(shared_job_q,shared_result_q):
+    global result_list
+    global num_results
+    global num_started_jobs
+    print "Num_started_jobs:", num_started_jobs
+    try:
+        num_jobs = int(raw_input("\nMPS> How many new job instances?"))                        
+        for l in xrange(num_started_jobs+1,num_started_jobs+num_jobs+1):
+            #print "Sending job#" + str(l)
+            #shared_job_q.put(str("print \"test: This is process #" + str(i) + "/" + str(N) + "\"\ndef crunching_zombie(n):\n\treturn n*n\nresult = crunching_zombie("+str(i)+")\nprint result\n"))
+            shared_job_q.put("result = sample_path(evoMPS_TDVP_Generic_Dissipative,"+str(l)+")")
+        print "\nPut", num_jobs, "jobs to the queue."
+        num_started_jobs+=num_jobs
+    except Exception, e:
+            print "Error: input does not appear to be integer",e
 
 def show_help():
     # Show the help
     print "\n\tFor now this only gives the list of commands.\n\n"
-    print "\tload: imports the 'tdvp_gen' and 'dissipative_montecarlo' modules when\n\tthey are changed."
-    print "\trun: start the computation by putting jobs into the queue."
-    print "\tlrun: load new code and start the computation directly after."
+    print "\tload: imports the 'tdvp_gen' and 'tdvp_gen_diss' modules when\n\tthey are changed."
+    print "\tadd: Add jobs to the queue."
+    print "\treset: Delete all collected results and purge the queue."
     print "\tanalysis: calculate the observables and plot some data."
     print "\thelp: this help screen."
-    print "\texit: shut down the computation server.\n\n"
-    print "\tThere are more commands but I cannot disclose them. Have fun!\n"
+    print "\texit: shut down the computation server.\n"
     print "\tYou can also set the system parameters by supplying python Code. A list\n\tof parameters follows.\n"
 
 def runserver():
     # Start a shared manager server and access its queues
     #manager = make_server_manager(port, authkey)
     
-    manager = make_server_manager(port,authkey)
-     
+    manager = make_server_manager(port,authkey)     
     shared_job_q = manager.get_job_q()
     shared_result_q = manager.get_result_q()
     codebase = manager.codebase()
-    load(manager,codebase)
-    #exec(codebase.__str__().__repr__())
-    print "done."
-
-    """
-    Initialize the shared variable codebase with the current versions
-    of the modules tdvp_gen and tdvp_diss
-    """
+    load(codebase)
     
-    while True:
-        try:
-            cmd = str(raw_input("\nMPS>"))
-            if cmd == "run":
-                do_calculations(shared_job_q,shared_result_q,codebase)
-                continue
-            if cmd == "lrun":
-                load(manager,codebase)
-                print "done."
-                do_calculations(shared_job_q,shared_result_q,codebase)
-                continue
-            if cmd == "load":
-                load(manager,codebase)
-                print "done."
-                continue
-            if cmd == "help":
-                show_help()
-                continue
-            if cmd == "zombie apocalypse":
-                print "Sorry, not implemented."
-                time.sleep(2)
-                print "Yet."
-                continue
-            if cmd == "analysis":
-                cdbs = manager.codebase().__str__() + "\n"
-                code = cdbs[12:-3]
-                decoded = code.replace("\\n","\n")
-                decoded = decoded.replace("\\r","\r")
-                execfile("analysis.py")
-                continue
-            if cmd == "exit":
-                break
-            print ""
-            exec(cmd)
-        except Exception, e:
-            print "Unexpected error:", sys.exc_info()[0]
-            print e
-            print "You can type 'help' for a list of commands."
+    global num_results
+    global result_list
+    
+    # Bring up the |MPS> prompt.
+    
+    while True:        
+        if shared_result_q.empty():
+            print "\n\nStatus: (" + str(len(result_list)) + "/" + str(num_started_jobs) + ") results collected."
+            try:
+                cmd = str(raw_input("\nMPS>"))
+                if cmd == "add" or cmd == "run":
+                    add_jobs(shared_job_q,shared_result_q)
+                    continue
+                if cmd == "stop" or cmd == "exit":
+                    print "==="
+                    print "Clearing up..."
+                    break
+                if cmd == "help":
+                    show_help()
+                    continue
+                if cmd == "zombie apocalypse":
+                    print "Sorry, not implemented."
+                    time.sleep(2)
+                    print "Yet."
+                    continue
+                if cmd == "reset":
+                    print "Not implemented."
+                    continue
+                if cmd == "analysis" or cmd == "ana":
+                    cdbs = manager.codebase().__str__() + "\n"
+                    code = cdbs[12:-3]
+                    decoded = code.replace("\\n","\n")
+                    decoded = decoded.replace("\\r","\r")
+                    execfile("../fh_analysis.py")
+                    continue
+                if cmd == "stop" or cmd == "exit":
+                    break
+                exec(cmd)
+            except Exception, e:
+                print "Unexpected error:", sys.exc_info()[0]
+                print e
+                print "You can type 'help' for a list of commands."
+            time.sleep(.1)
+            continue
+        else:
+            result_list.append(shared_result_q.get())
+            continue
     
     # Sleep a bit before shutting down the server - to give clients time to
     # realize the job queue is empty and exit in an orderly way.
     print "SERVER shutting down."
-    time.sleep(5)
+    time.sleep(2)
     manager.shutdown()
     print "Shutdown complete. Bye bye."
 
@@ -175,32 +205,43 @@ class Codebase():
 
     codebase = ""
     
-    def __init__(self,manager):
-        'Constructor of the Codebase class'
-        #self.update(manager)
+    def __init__(self):
+        pass
    
     def get_codebase(self):
         return self.codebase
 
     
-def load(manager, codebase):
+def load(codebase):
     'Update method. When the program code of the tdvp suite got changed, call this.'
     
     print "Loading program code into shared memory..."
     
     # Load the current versions of tdvp_diss and tdvp_gen
     
-    tdvp_gen_fh = open("tdvp_gen.py","r")
-    tdvp_gen = tdvp_gen_fh.read()
-    tdvp_gen_fh.close()
+    tdvp_com = ""
+    #tdvp_com_fh = open("tdvp_common.py","r")
+    #tdvp_com = tdvp_com_fh.read()
+    #tdvp_com_fh.close()    
+    
+    tdvp_gen = ""
+    #tdvp_gen_fh = open("tdvp_gen.py","r")
+    #tdvp_gen = tdvp_gen_fh.read()
+    #tdvp_gen_fh.close()
     #exec(tdvp_gen)
     
-    tdvp_diss_fh = open("tdvp_diss.py","r")
+    #tdvp_diss_fh = open("../tdvp_gen_diss_ash_old.py","r")
+    #tdvp_diss_fh = open("tdvp_gen_diss_fermi_hubbard_2d.py","r")
+    tdvp_diss_fh = open("tdvp_gen_diss_fermi_hubbard_new.py","r")
+    #tdvp_diss_fh = open("D:/promotion/repos/mpsampling/evoMPS/evoMPS/tdvp_gen_diss.py","r")
     tdvp_diss = tdvp_diss_fh.read()
     tdvp_diss_fh.close()
     #exec(tdvp_diss)
 
-    codec = tdvp_gen + str("\n\r") + tdvp_diss
+    #codec = tdvp_com + str("\n\r") + tdvp_gen + str("\n\r") + tdvp_diss
+    codec = tdvp_diss
+    
+    exec(codec)
 
     print "Code loaded. Distributing and syncing clients..."
       
